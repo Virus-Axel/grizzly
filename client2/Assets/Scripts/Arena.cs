a14979839b33df4e18319f442b976a1796e6d9ec
Axel Benjaminsson <axel.benjaminsson@live.se>
2023-02-09 13:59:02 +0100

    Make capsules take turns to fight

--- client2/Assets/Scripts/Arena.cs
+++ client2/Assets/Scripts/Arena.cs
@@ -4,21 +4,43 @@ using UnityEngine;
 
 public class Arena : MonoBehaviour
 {
+    int turn = 0;
+
+    public Bear blueBear = null;
+    public Bear redBear = null;
+
     // Start is called before the first frame update
     void Start()
     {
+        testBears();
+    }
 
-        GameObject bearObject = GameObject.Find("Bear");
-        GameObject opponentObject = GameObject.Find("Opponent");
-        Bear bear = (Bear) bearObject.GetComponent(typeof(Bear));
-        Bear opponent = (Bear) opponentObject.GetComponent(typeof(Bear));
-        bear.target = opponentObject;
-        opponent.target = bearObject;
+    void testBears()
+    {
+        blueBear.UpgradeAbility(Bear.ABILITY.PUNCH);
+        blueBear.EquipAbility(Bear.ABILITY.PUNCH);
+        blueBear.UpgradeAbility(Bear.ABILITY.TEST1);
+        blueBear.EquipAbility(Bear.ABILITY.TEST1);
+
+        redBear.UpgradeAbility(Bear.ABILITY.PUNCH);
+        redBear.EquipAbility(Bear.ABILITY.PUNCH);
+        redBear.UpgradeAbility(Bear.ABILITY.TEST1);
+        redBear.EquipAbility(Bear.ABILITY.TEST1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(blueBear.state == Bear.BEAR_STATE.WALKING && redBear.currentAction == Bear.ABILITY.NONE && blueBear.currentAction == Bear.ABILITY.NONE && turn % 2 == 0)
+        {
+            blueBear.Act();
+            turn += 1;
+        }
+        else if(redBear.state == Bear.BEAR_STATE.WALKING && redBear.currentAction == Bear.ABILITY.NONE && blueBear.currentAction == Bear.ABILITY.NONE && turn % 2 == 1)
+        {
+            redBear.Act();
+            turn += 1;
+        }
         
     }
 }
--- client2/Assets/Scripts/Bear.cs
+++ client2/Assets/Scripts/Bear.cs
@@ -6,6 +6,18 @@ using UnityEngine;
 public class Bear : MonoBehaviour
 {
     const int NUMBER_OF_EQUIPPED_ABILITIES = 3;
+    const float RUN_DISTANCE = 5.0F;
+    const float WALK_DISTANCE = 2.0F;
+    const float ATTACK_RANGE = 2.5F;
+
+    const float STAGGERED_TIME = 1.0F;
+    const float KNOCKOUT_TIME = 3.0F;
+
+    const float RUN_SPEED = 0.2F;
+    const float WALK_SPEED = 0.1F;
+
+    const float STAMINA_PENALITY = 0.05F;
+    const float STAMINA_RECOVERY = 0.01F;
 
     float brainSize = 1.0F;
     float lowerArmSize = 1.0F;
@@ -16,8 +28,14 @@ public class Bear : MonoBehaviour
     float upperBodySize = 1.0F;
     float lowerBodySize = 1.0F;
     float stamina = 0.5F;
+    float health = 1.0F;
+    public bool dead = false;
 
-    enum ABILITY
+    float timer = 0.0F;
+
+    float attackTime = 1.0F;
+
+    public enum ABILITY
     {
         NONE,
         PUNCH,
@@ -28,29 +46,41 @@ public class Bear : MonoBehaviour
         NO_ABILITIES,
     };
 
-    enum BEAR_STATE
+    public enum BEAR_STATE
     {
         WALKING,
         ATTACKING,
         STAGGERED,
+        KNOCKOUT,
     }
 
-    BEAR_STATE state = BEAR_STATE.WALKING;
-    ABILITY currentAction = ABILITY.NONE;
+    public BEAR_STATE state = BEAR_STATE.WALKING;
+    public ABILITY currentAction = ABILITY.NONE;
 
     int[] abilityLevels = new int[(int)ABILITY.NO_ABILITIES - 1]{0, 0, 0, 0, 0};
 
-    List<ABILITY> equippedAbilities;
+    List<ABILITY> equippedAbilities = new List<ABILITY>{};
 
-    public GameObject target;
+    public Bear target = null;
 
-    void EquipAbility(ABILITY ability, int index = -1)
+    public void Act()
     {
-        if ((int)ability > abilityLevels.Length)
+        currentAction = ChoseAction();
+        UnityEngine.Debug.Log(currentAction);
+        if (currentAction != ABILITY.NONE)
+        {
+            this.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    public void EquipAbility(ABILITY ability, int index = -1)
+    {
+        int levelIndex = (int) ability - 1;
+        if (levelIndex >= abilityLevels.Length || levelIndex < 0)
         {
             return;
         }
-        if (abilityLevels[(int) ability] <= 0)
+        if (abilityLevels[levelIndex] <= 0)
         {
             return;
         }
@@ -68,7 +98,7 @@ public class Bear : MonoBehaviour
         }
     }
 
-    void UpgradeAbility(ABILITY ability)
+    public void UpgradeAbility(ABILITY ability)
     {
         if (ability == ABILITY.NONE || ability == ABILITY.NO_ABILITIES){
             return;
@@ -81,7 +111,7 @@ public class Bear : MonoBehaviour
         float randomVal = Random.Range(0.0F, 1.0F);
         if (randomVal > stamina || equippedAbilities.Count <= 0)
         {
-            stamina += 0.1F;
+            stamina += STAMINA_RECOVERY;
             return ABILITY.NONE;
         }
 
@@ -90,23 +120,84 @@ public class Bear : MonoBehaviour
         return equippedAbilities[randomIndex];
     }
 
-    public void SetTarget(GameObject newTarget)
+    public void SetTarget(Bear newTarget)
     {
         target = newTarget;
+    }
+
+    public void HitBear(float damage, bool heavy=false)
+    {
+        health -= damage;
+        if (health <= 0.0F)
+        {
+            dead = true;
+        }
+        if(heavy)
+        {
+            SetState(BEAR_STATE.KNOCKOUT);
+        }
+        else
+        {
+            SetState(BEAR_STATE.STAGGERED);
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        target = null;
-        equippedAbilities = new List<ABILITY>{};
+    }
+
+    void SetState(BEAR_STATE newState)
+    {
+        state = newState;
+        if(state == BEAR_STATE.KNOCKOUT)
+        {
+            this.GetComponent<Renderer>().material.color = Color.black;
+        }
+        else if(state == BEAR_STATE.WALKING)
+        {
+            this.GetComponent<Renderer>().material.color = Color.white;
+        }
+        else if(state == BEAR_STATE.ATTACKING)
+        {
+            
+        }
+        else if (state == BEAR_STATE.STAGGERED)
+        {
+            this.GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        else if (state == BEAR_STATE.KNOCKOUT)
+        {
+            this.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    void TryAttack()
+    {
+        if(currentAction == ABILITY.NONE)
+        {
+            return;
+        }
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        if(distance < ATTACK_RANGE && target.state != BEAR_STATE.KNOCKOUT)
+        {
+            SetState(BEAR_STATE.ATTACKING);
+            this.GetComponent<Renderer>().material.color = Color.yellow;
+            stamina -= STAMINA_PENALITY;
+        }
     }
 
     void WalkStrategically()
     {
         float distance = Vector3.Distance(transform.position, target.transform.position);
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 1.0F);
-        UnityEngine.Debug.Log("Why");
+        if(distance > RUN_DISTANCE)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, RUN_SPEED * Time.deltaTime);
+        }
+        else if (distance > WALK_DISTANCE)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, WALK_SPEED * Time.deltaTime);
+        }
     }
 
     // Update is called once per frame
@@ -122,8 +213,34 @@ public class Bear : MonoBehaviour
         {
             case BEAR_STATE.WALKING:
                 WalkStrategically();
+                TryAttack();
                 break;
-            
+            case BEAR_STATE.ATTACKING:
+                timer += Time.deltaTime;
+                if(timer >= attackTime)
+                {
+                    timer = 0.0F;
+                    SetState(BEAR_STATE.WALKING);
+                    target.HitBear(0.2F);
+                    currentAction = ABILITY.NONE;
+                }
+                break;
+            case BEAR_STATE.STAGGERED:
+                timer += Time.deltaTime;
+                if(timer >= STAGGERED_TIME)
+                {
+                    timer = 0.0F;
+                    SetState(BEAR_STATE.WALKING);
+                }
+                break;
+            case BEAR_STATE.KNOCKOUT:
+                timer += Time.deltaTime;
+                if(timer >= KNOCKOUT_TIME)
+                {
+                    timer = 0.0F;
+                    SetState(BEAR_STATE.WALKING);
+                }
+                break;
         }
         
     }
